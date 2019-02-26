9b09e9bda0fef9f42418c171c401492265a028f1
gmp26 <gmp26@cam.ac.uk>
2019-02-26 10:35:42 +0000

    pulled in from embedded prostate version

--- src/wc_rum_lib/numeric_input.cljs
+++ src/wc_rum_lib/numeric_input.cljs
@@ -1,9 +1,11 @@
 (ns wc-rum-lib.numeric-input
+  (:refer-clojure :exclude [max min])
   (:require [rum.core :as rum]
+            [clojure.string :refer [split trim]]
             [cljs-css-modules.macro :refer-macros [defstyle]]))
 
 (defstyle style
-  ["#numeric-input" {:width      "130px"
+  [".numeric-input" {:width      "130px"
                      :tab-index  1
                      :selectable true}
    [".incdec"
@@ -17,120 +19,198 @@
 
 (defn error? [value] (or (nil? value) (= "" value) (js/isNaN value)))
 
+;;
+;; Numeric values are always stored in state as strings, but processed as numerics
+;;
+(defn str-to-num [s] (js/parseFloat s))
+(comment
+  (str-to-num "3.5")                                        ;=> 3.5
+  (str-to-num 3.5)                                          ;=> 3.5
+  )
+
+(def epsilon 1e-8)
+
+(defn near-integer? [n]
+  (< (js/Math.abs (- n (js/Math.round n))) epsilon))
+
+(defn trim-trailing-zero [s]
+  (if-let [[m m1] (re-matches #"(.*\.\d)\d+" s)]
+    m1 s))
+
+(defn at-precision [n precision]
+  (cond
+    (= 0 precision)
+    ; display as integer
+    (str (js/Math.ceil n))
+
+    (= 3 precision)
+    ; flexible display up to 3dp
+    (if (near-integer? n)
+      (str (js/floor n))
+      (-> n
+          (.toPrecision (js/Number. 3))
+          (trim-trailing-zero)))
+
+    (= 2 precision)
+    ; display with 2dp always
+    (.toFixed (js/Number. n) 2)
+
+    (= 1 precision)
+    ; display with 1dp always
+    (.toFixed (js/Number. n) 1)
+    )
+  )
+
+(defn num-to-str
+  ([n]
+    ; default to integer
+   (num-to-str n 0))
+  ([n precision]
+   (if (string? n)
+     (do
+       ;(println n " returning unchanged string")
+       n)
+     (if (js/isNaN n)
+       ""
+       (if (near-integer? n)
+         (str (js/Math.floor n))
+         (if precision
+           (do
+             ;(println "num-to-str precision " n precision)
+             (at-precision n precision))
+           (at-precision n 0))
+         #_(-> n
+               (.toPrecision (js/Number. 3))
+               (trim-trailing-zero)))))))
+
 ; this can be global as there is only one input under focus at any one time
 (def timer (atom nil))
 
-(defn handle-numeric-input [min max color onChange e]
+(defn validate [value nmin nmax step]
+  (let [value (str-to-num value)
+        nmin (if (fn? nmin) @(nmin) nmin)
+        nmax (if (fn? nmax) @(nmax) nmax)                   ;(if (keyword? nmax) @(input-cursor nmax) nmax)
+        val-1 (if (js/isNaN value)                          ; is value blank?
+                (if (pos? step)                             ; is it an increment?
+                  (dec nmin)                                ; yes - go to one less than minimum (we'll increment later)
+                  (if (neg? step)                           ; is it a decrement?
+                    (inc nmax)                              ; yes - got to one more than maximum (we'll decrement later)
+                    nmin                                    ; no
+                    ))
+                value)
+        val-2 (+ step val-1)                                ; do the increment
 
-  (let [el (-> e .-target)
-        value (js/parseInt (.. e -target -value))
-        value (if (error? value) "" value)
-        ]
-    ; _very_ tricky to force in-range values that are editable here since they are not in-range while
-    ; partially entered. Hence the 500ms timeout needed to allow entry of a valid value. We aim to flag
-    ; incomplete or bad values in red, and delete them after 500ms.
-    #_(if (and (not (error? value)) (<= min value max))
-      ; in range
-      (when @timer (js/clearTimeout @timer))
+        val-3 (if (< val-2 nmin)                            ; is it too small?
+                (str (num-to-str val-2) ":" val-2)          ; yes
+                (if (> val-2 nmax)                          ; no; is it too big?
+                  (str (num-to-str val-2) ":" val-2)        ; yes, return good and bad values, in colon separated string
+                  val-2))]                                  ; no
+    ;(println "(validate " value nmin nmax step ") = " val-3)
+    val-3))
 
-      ; out of range
-      (let [v ""
-            src-element (-> e .-nativeEvent .-srcElement)]
-        (reset! timer (js/setTimeout #(do
-                                        ; this block must be idempotent as it may be called multiple times
-                                        (goog.object.set el "value" "")
-                                        (goog.object.set (goog.object.get src-element "style") "color" color)
-                                        (onChange "")
-                                        ) 500))
-        ;(onChange "")
-        ))
+(defn handle-inc [value onChange nmin nmax precision step]
+  (let [v (validate value nmin nmax step)]
+    ;(js/console.log "onChange " v)
+    (onChange (num-to-str v precision))))
 
-    ; dispatch the clipped value.
-    (onChange (str value)))
-  )
 
-(defn handle-inc [value onChange min max step]
-  (when (not= 0 step)
-    (let [val-1 (if (= value "")
-                  (if (pos? step)
-                    (dec min)
-                    (if (neg? step) (inc max) ""))
-                  (js/parseInt value))
-          val-2 (if (error? val-1) "" (+ step val-1))
-          val-3 (if (< val-2 min) min (if (> val-2 max) max val-2))]
-      (onChange (str val-3))
-      )))
+(defn handle-typed-input [nmin nmax precision onChange e]
+  (let [value (.. (-> e .-target) -value)]
+    ;(.log js/console value)
+    (if (re-matches #"\s*\d*\.?\d*\s*" value)
+      (onChange (num-to-str (validate (str-to-num value) nmin nmax 0) precision))
+      (onChange (num-to-str ##NaN)))
+    ))
 
-(rum/defcs inc-dec-button < rum/static (rum/local nil ::timer)
+(defn update-value [value nmin nmax precision step onChange]
+  (handle-inc value onChange nmin nmax precision step))
+
+#_(defn clear-timer [state]
+    (js/clearInterval @(::timer state))
+    (reset! (::timer state) nil))
+
+
+(rum/defcs inc-dec-button < rum/static rum/reactive (rum/local nil ::timer)
   [state
-   {:keys [cursor increment onChange min max]
+   {:keys [cursor increment onChange min max nmin nmax precision]
     :as   props}]
-  (let [start-timer (fn [e] (js/setInterval #(handle-inc @cursor onChange min max increment) 333))]
+  (let [value (str-to-num (rum/react cursor))
+        ;
+        ; Removing the timer as I don't have a reliable trigger to cancel it yet. Maybe we should use global state in
+        ; case of a component re initialisation?
+        ;
+        #_#_start-timer (fn [e] (js/setIntervak #(handle-inc @cursor onChange nmin nmax increment) 250))]
     [:span {:class-name "incdec"}
-     [:a {:class-name    (str (if (pos? increment) "right" "left") " btn btn-default") ;:style
-          #_{:border-top-right-radius    left-r
-             :border-bottom-right-radius left-r
-             :border-top-left-radius     right-r
-             :border-bottom-left-radius  right-r
-             :width                      "36px"}
-          :aria-hidden   "true"
-          :tab-index     -1
-          :on-mouse-down #(do (reset! (::timer state) (start-timer %))
-                              ;(handle-inc @cursor onChange min max increment)
-                              )
-          :on-mouse-up   #(do (js/clearInterval @(::timer state))
-                              (reset! (::timer state) nil))
-          :on-mouse-out  #(do (js/clearInterval @(::timer state))
-                              (reset! (::timer state) nil))
-          :on-click      #(handle-inc @cursor onChange min max increment)}
+     ;
+     ;todo: I was using a buttonified [:a]  here. Keep an eye out for issues with change to more semantic [:button]
+     ;
+     [:button {:class-name  (str (if (pos? increment) "right" "left") " btn btn-default ")
+               :aria-hidden "true"
+               :disabled    (if (pos? increment)
+                              (if (>= value (str-to-num (if (fn? max) (rum/react (max)) max))) "disabled" nil)
+                              (if (<= value nmin) "disabled" nil))
+               :tab-index   -1
+               ;:on-mouse-down #(do (reset! (::timer state) (start-timer %)))
+               ;:on-mouse-up   #(clear-timer state)
+               ;:on-mouse-out  #(clear-timer state)
+               ;:on-mouse-leave  #(clear-timer state)
+               :on-click    #(do                            ;(clear-timer state)
+                               (update-value @cursor nmin nmax precision increment onChange))}
       (if (pos? increment) "+" "–")]]))
 
-#_(def echo-update {:did-update (fn [state]
-                                (let [args (:rum/args state)
-                                      comp (:rum/react-component state)
-                                      node (js/ReactDOM.findDOMNode comp)]
-                                  (assoc state ::node node))
-                                )})
 
 
 (rum/defc numeric-input < rum/static rum/reactive           ;echo-update
-  [{:keys [input-ref onChange min max error-color color] :or {error-color "red" color "black"} :as props}]
+  [{:keys [key input-ref onChange min max error-color color precision] :or {error-color "red" color "black"} :as props}]
 
+  ;(.log js/console "key: " input-ref " precision: " precision)
 
-  (let [value (rum/react input-ref)
-        min (if (fn? min) (min) min)
-        max (if (fn? max) (max) max)]
-    ; min and max can be functions, so in particular they may be calls to rum/react
+  ;(js/console.log "props: " props)
+  (let [[good bad] (split (rum/react input-ref) #":")
+        #_#_value (str-to-num (rum/react input-ref))
+        value (str-to-num good)
+        nmin (str-to-num (if (fn? min) (rum/react (min)) min))
+        nmax (str-to-num (if (fn? max) (rum/react (max)) max))
 
-    [:div {:id          "numeric-input"
+        mutate (fn [e]
+                 ;(js/console.log "mutating, :nmin = " nmin " value = " value " bad = " bad)
+                 (handle-typed-input
+                   (str-to-num (if (fn? min) @(min) min))   ; include precision?
+                   (str-to-num (if (fn? max) @(max) max))   ; include precision?
+                   precision
+                   onChange e))]
+
+    [:div {:class       "numeric-input"
            :style       {:width      "130px"
                          :tab-index  1
                          :selectable true}
            :on-key-down #(let [key-code (.. % -nativeEvent -code)]
-                           (handle-inc value onChange min max
-                             (cond
-                               (= "ArrowRight" key-code) 5
-                               (= "ArrowUp" key-code) 1
-                               (= "ArrowDown" key-code) -1
-                               (= "ArrowLeft" key-code) -5
-                               :else 0)))}
+                           (when (#{"ArrowUp" "ArrowDown"} key-code)
+                             (.preventDefault %))
+                           (update-value value nmin nmax precision
+                                         (cond
+                                           (= "ArrowUp" key-code) 1
+                                           (= "ArrowDown" key-code) -1
+                                           :else 0)
+                                         onChange))}
      [:button-group.form-control
-      (inc-dec-button (assoc props :increment -1 :cursor input-ref))
+      (inc-dec-button (assoc props :nmin nmin :nmax nmax :precision precision :increment -1 :cursor input-ref))
       [:input
        {:type      "text"
-        :value     (if (nil? value) "" value)
-        :on-click  (partial handle-numeric-input min max color onChange)
-        :on-change (partial handle-numeric-input min max color onChange)
+        :value     good                                     ;(num-to-str value)
+        :id        key
+        :on-click  mutate
+        :on-change mutate
         :style     {:width            "58px" :height "36px" :font-size "14px"
                     :border-top       "2px solid #ddd"
                     :border-left      "2px solid #ddd"
-                    :background-color (if (not= value "") "#CCEEF8" "#fff")
-                    :color            (if (<= min value max) color error-color)
+                    :background-color (if (js/isNaN value) "#fff" "#CCEEF8")
+                    :color            (if (nil? bad) color error-color)
                     :padding          "0 0 4px 0"
                     :text-align       "center"
                     #_#_:font-weight "bold"}
         }]
-      (inc-dec-button (assoc props :increment 1 :cursor input-ref))
+      (inc-dec-button (assoc props :nmin nmin :nmax nmax :precision precision :increment 1 :cursor input-ref))
       ]]
 
     ))
