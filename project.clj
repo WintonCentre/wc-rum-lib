ad9923509541312c20ab85522b9f2adc619a595c
Jin Park <JinPark.Han@gmail.com>
2019-03-07 17:07:21 +0000

    Bump version after numeric input bug fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wc-rum-lib "0.1.14"
+(defproject wc-rum-lib "0.1.15"
   :description "FIXME: write description"
   :url "https://github.com/wintoncentre/wc-rum-lib"
   :license {:name "Eclipse Public License"
